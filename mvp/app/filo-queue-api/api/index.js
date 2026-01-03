49d95db9de2202cc6837e16de40fcefc94042190
Welliton Dev <gioriwelliton47@gmail.com>
2026-01-02 20:03:58 -0400

    vercel

--- mvp/app/filo-queue-api/api/index.js
+++
@@ -1,4 +0,0 @@
-const createApp = require('../src/app');
-
-// Export the Express app for Vercel serverless
-module.exports = createApp();
--- mvp/app/filo-queue-api/src/index.js
+++ mvp/app/filo-queue-api/src/index.js
@@ -1,4 +1,4 @@
-const createApp = require('./app');
+const createApp = require('./server/app');
 const app = createApp();
 
 const port = process.env.PORT || 3000;
